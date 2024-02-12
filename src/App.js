2f65e98b201441ff8ec7a52c901f0c3fba2a6d74
Izyk <lance_hemphill1234@yahoo.com>
2024-02-11 20:17:22 -0500

    styling for light mode is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 
 import './App.css';
 import Countries from './components/countries';
+import { useState } from 'react';
+
 
 function App() {
   return (
     <div className="App">
-        <Countries />
+          <div className="componentContainer">
+            <div className="subContainer">
+              <p>Where in the world?</p>
+              <button>Dark Mode</button>
+            </div>
+          </div>
+      <Countries />
     </div>
   );
 }
--- src/components/countries.js
+++ src/components/countries.js
@@ -10,85 +10,63 @@ const Countries = () => {
 
 
     useEffect(() => {
-        if (input.length === 0 && region === "")  {
-            getAllCountries()
-        }
-        else if (input.length > 0){
-            filterByName()
-        }
-        else if (input.length > 0 && region !== "") {
-            filterByRegionAndName()
-        }
+        async function fetchCountries() {
+            let url = 'https://restcountries.com/v3.1/all'
 
-    }, [input])
+            if(input) {
+                url = `https://restcountries.com/v3.1/name/${input}`
+            }
+            else if (region) {
+                url = `https://restcountries.com/v3.1/region/${region}`
+            }
 
-    useEffect(() => {
-        if(region !== "") {
-            filterByRegion()
-        }
-        else if (region === "") {
-            getAllCountries()
-        }
-    }, [region])
+            try {
+                const response = await fetch(url) 
+                let result = await response.json();
+
+                
 
 
-    async function getAllCountries() {
-        try {
-            const response = await fetch('https://restcountries.com/v3.1/all')
-            const result = await response.json();
-            setData(result)
-            console.log(result)
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-
-    async function filterByName() {
-        try {
-            const response = await fetch(`https://restcountries.com/v3.1/name/${input}`)
-            const result = await response.json();
-            setData(result.filter(country => {
-                if(country.name !== input) {
-                    return "This does not equal a country"
+                if(input && region) {
+                    result = result.filter(country => country.region.includes(region))
+                    setData(result)
                 }
 
-                return country
-            }))
-            console.log(result)
-        } catch (error) {
-            console.log(error)
+                if(input) {
+                    result = result.filter(country => country.name)
+                    setData(result)
+                }
+
+                setData(result)
+
+            } catch(error) {
+                console.log(error, "error")
+            }
         }
-    }
 
-    async function filterByRegion() {
-        try {
-            const response = await fetch(`https://restcountries.com/v3.1/region/${region}`)
-            const result = await response.json()
-            setData(result)
+        fetchCountries()
 
-        } catch (error) {
-            console.log(error)
-        }
-    }
+    }, [input, region])
 
-    async function filterByRegionAndName() {
-        const response = await fetch(`https://restcountries.com/v3.1/name/${input}`)
-        const result = await response.json()
-        if(input !== result.name) {
-            console.log("Does not equal country")
-            return null
-        }
-        setInput(result.filter(country => country.region === region))
-    }
 
-    console.log(region)
+    
+
+
+
     return (
-        <div>
+        <div className={styles.componentContainer}>
             <div className={styles.inputContainer}>
-                <input type='text' onChange={(e) => setInput(e.target.value)}/>
-                <select id='regionSelection' onChange={(e) => setRegion(e.target.value)} defaultValue={region}>
-                    <option value=""></option>
+                <input placeholder="Search Country" id='countryInput' type='text' onChange={(e) => setInput(e.target.value)}/>
+                <select className={styles.selectRegion} id='regionSelection' onChange={(e) => {
+
+                    setRegion(e.target.value)
+                    setInput("")
+                    document.getElementById('countryInput').value = ""
+                    
+                    }} defaultValue={region}>
+
+                        
+                    <option value="">Select By Region</option>
                     <option value="Americas">Americas</option>
                     <option value="Europe">Europe</option>
                     <option value="Asia">Asia</option>
